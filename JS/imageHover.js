b497b12dfe47f260ac4c3eedc8c06d5311ae5493
joshneal <40867140+joshneal@users.noreply.github.com>
2018-07-16 18:30:35 -0700

    Add files via upload

--- JS/imageHover.js
+++ JS/imageHover.js
@@ -46,4 +46,21 @@
 		function mouseOut4 () {
 			document.getElementById("contact").src = "Images/Icons/Contact Me.jpg";
 		}
+
+
+		//Collapsible List at bottom
+		var coll = document.getElementsByClassName("collapsible");
+		var i;
+
+		for (i = 0; i < coll.length; i++) {
+		  coll[i].addEventListener("click", function() {
+		    this.classList.toggle("active");
+		    var content = this.nextElementSibling;
+		    if (content.style.maxHeight){
+		      content.style.maxHeight = null;
+		    } else {
+		      content.style.maxHeight = content.scrollHeight + "px";
+		    } 
+		  });
+		}
 	}
